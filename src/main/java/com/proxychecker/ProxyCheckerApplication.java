09cf639689f3b0d3322647959a5fb2f33ad1e96d
w33 <strokonage@yandex.ru>
2024-11-23 15:43:42 +0300

    #XXX added flags.png

--- src/main/java/com/proxychecker/ProxyCheckerApplication.java
+++ src/main/java/com/proxychecker/ProxyCheckerApplication.java
@@ -1,28 +1,13 @@
 package com.proxychecker;
 
-import com.proxychecker.service.ProxyCheckerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProxyCheckerApplication /*implements CommandLineRunner*/ {
-    @Value( "${proxy:socks4}" )
-    private String proxy;
-
-    @Autowired
-    private ProxyCheckerService proxyCheckerService;
+public class ProxyCheckerApplication {
 
     public static void main( String[] args ) {
         SpringApplication.run( ProxyCheckerApplication.class, args );
     }
-
-    /*@Override
-    public void run( String... args ) throws Exception {
-        proxyCheckerService.checkProxies( proxy );
-    }*/
-
 
 }
