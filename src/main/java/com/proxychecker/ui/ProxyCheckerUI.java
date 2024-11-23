3ce7c8e9c3c07a01064619118274a92ad2b7cfc2
w33 <strokonage@yandex.ru>
2024-11-23 13:59:56 +0300

    #XXX added images

--- src/main/java/com/proxychecker/ui/ProxyCheckerUI.java
+++ src/main/java/com/proxychecker/ui/ProxyCheckerUI.java
@@ -4,15 +4,20 @@ import com.proxychecker.dto.ProxyDto;
 import com.proxychecker.service.ProxyCheckerService;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 import static com.proxychecker.constants.AppConstants.RESOURCE_GITHUB;
 import static com.proxychecker.constants.AppConstants.RESOURCE_PROXY_LIST;
@@ -34,24 +39,59 @@ public class ProxyCheckerUI extends VerticalLayout {
     public ProxyCheckerUI( ProxyCheckerService proxyCheckerService ) {
         this.proxyCheckerService = proxyCheckerService;
 
-        // Инициализация компонентов UI
-        proxyGrid = new Grid<>( ProxyDto.class );
-        proxyGrid.setColumns( "host", "port", "proxyType", "responseTime", "country" );
-        proxyGrid.addThemeVariants( GridVariant.LUMO_NO_BORDER );
-
-        // Настройка отображения времени отклика в секундах с тремя знаками после запятой
-        proxyGrid.getColumnByKey( "responseTime" )
-                .setHeader( "Время отклика (с)" )
-                .setComparator( ProxyDto::getResponseTime );
-
         resultDiv = new Div();
+        proxyGrid = new Grid<>( ProxyDto.class );
+        proxyGrid.removeAllColumns();
+
+        proxyGrid.addColumn( new ComponentRenderer<>( proxy -> {
+                    Image statusImage = Objects.nonNull( proxy.getResponseTime() ) && proxy.getResponseTime().compareTo( BigDecimal.valueOf( 0.300 ) ) < 0
+                            ? new Image( "images/ok.png", "OK" )
+                            : new Image( "images/warning.png", "Warning" );
+
+                    statusImage.setWidth( "14px" );
+                    statusImage.setHeight( "14px" );
+                    return statusImage;
+                } ) ).setHeader( "Статус" )
+                .setTextAlign( ColumnTextAlign.CENTER )
+                .setWidth( "75px" )
+                .setFlexGrow( 0 );
+
+        proxyGrid.addColumn( proxy -> proxy.getHost() + ":" + proxy.getPort() )
+                .setHeader( "IP:Порт" )
+                .setTextAlign( ColumnTextAlign.CENTER )
+                .setWidth( "225px" )
+                .setFlexGrow( 0 );
+
+        proxyGrid.addColumn( ProxyDto::getProxyType )
+                .setHeader( "Тип прокси" )
+                .setTextAlign( ColumnTextAlign.CENTER )
+                .setComparator( ProxyDto::getProxyType )
+                .setWidth( "125px" )
+                .setFlexGrow( 0 );
+
+        proxyGrid.addColumn( ProxyDto::getResponseTime )
+                .setHeader( "Время отклика (с)" )
+                .setTextAlign( ColumnTextAlign.CENTER )
+                .setComparator( ProxyDto::getResponseTime )
+                .setWidth( "175px" )
+                .setFlexGrow( 0 );
+
+        proxyGrid.addColumn( ProxyDto::getCountry )
+                .setHeader( "Страна" )
+                .setTextAlign( ColumnTextAlign.CENTER )
+                .setComparator( ProxyDto::getCountry )
+                .setWidth( "100px" )
+                .setFlexGrow( 0 );
+
+        proxyGrid.addThemeVariants( GridVariant.LUMO_NO_BORDER );
+        proxyGrid.setHeight( "700px" );
 
         proxyTypeComboBox = new ComboBox<>( "Тип прокси" );
-        proxyTypeComboBox.setItems( "HTTP", "SOCKS4", "SOCKS5" ); // Пример типов
+        proxyTypeComboBox.setItems( "HTTP", "SOCKS4", "SOCKS5" );
         proxyTypeComboBox.setPlaceholder( "Выберите тип прокси" );
 
         resourceComboBox = new ComboBox<>( "Ресурс для подключения" );
-        resourceComboBox.setItems( RESOURCE_GITHUB, RESOURCE_PROXY_LIST ); // Пример ресурсов
+        resourceComboBox.setItems( RESOURCE_GITHUB, RESOURCE_PROXY_LIST );
         resourceComboBox.setPlaceholder( "Выберите ресурс" );
 
         Button checkButton = new Button( "Проверить прокси" );
